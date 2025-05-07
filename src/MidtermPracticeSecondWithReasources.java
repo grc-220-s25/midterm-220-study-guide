9ab7cc377b7064668f28c54ff05c1f598e1834df
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-05-06 19:25:29 -0700

    second attempt with resources, countLongWords, findAverage, countOddNumbers completed

--- src/MidtermPracticeSecondWithReasources.java
+++ src/MidtermPracticeSecondWithReasources.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,8 +10,35 @@ public class MidtermPracticeSecondWithReasources {
         // After implementing a method, call it here with
         // some sample input and print the result to
         // check if it works. Fix any issues you find.
-        List<String> newStringList = new ArrayList<>(Arrays.asList("apple", "banana", "kiwi"));
-        System.out.println(findLongestWord(newStringList));;
+
+        // List<String> newStringList = new ArrayList<>(Arrays.asList("apple", "banana", "kiwi"));
+        // System.out.println(findLongestWord(newStringList));;
+
+        // Map<String, Integer> wordScores = new HashMap<>();
+        // wordScores.put("why", 7);
+        // wordScores.put("hello", 8);
+        // wordScores.put("brilliant", 99);
+        // wordScores.put("world", 15);
+        // wordScores.put("amazing", 17);
+
+        // System.out.println(countLongWords(wordScores));
+
+        // float[] floatList = {1.5f, 3.5f, 2.0f, 3.0f};
+        // System.out.println(findAverage(floatList));
+
+        // Map<String,Integer> isOdd = new HashMap<>();
+        // isOdd.put("a", 7);
+        // isOdd.put("x", 4);
+        // isOdd.put("z", 5);
+
+        // System.out.println(countOddNumbers(isOdd));
+        int[] isDivbyThree = {3, 6, 9};
+        int[] isNotDivbyThree = {3, 4, 6, 9};
+
+        System.out.println(allDivisibleBy3(isDivbyThree));
+        System.out.println(allDivisibleBy3(isNotDivbyThree));
+
+
     }
 
     /**
@@ -45,11 +73,17 @@ public class MidtermPracticeSecondWithReasources {
      * Example: input: ["why"=7, "hello"=8, "brilliant"=99, "world"=15, "amazing"=17] -> output: 2
      */
     public static int countLongWords(Map<String, Integer> map) {
-        return -1;
+        int count = 0;
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+                if (entry.getKey().length() > 5) {
+                    count++;
+                }
+        }
+        return count;
     }
     // --- Attempt 2 (With Resources, 10 min) ---
-    // Time Completed:
-    // Notes:
+    // Time Completed: 4:01 seconds left
+    // Notes: I think I did well I just need to memorize a few things and get a better understanding.
 
 
 
@@ -62,11 +96,17 @@ public class MidtermPracticeSecondWithReasources {
      * Example: input: [1.5, 3.5, 2.0, 3.0] -> output: 2.5
      */
     public static double findAverage(float[] arr) {
-        return -1.0;
+        float sum = 0;
+        int count = arr.length;
+
+        for(float decimal : arr){
+            sum += decimal;
+        }
+        return sum/count;
     }
     // --- Attempt 2 (With Resources, 10 min) ---
-    // Time Completed:
-    // Notes:
+    // Time Completed: 7:26 seconds remaining
+    // Notes: feeling really good about this one I think I have a very good grasp on this one.
 
 
 
@@ -78,11 +118,17 @@ public class MidtermPracticeSecondWithReasources {
      * Example: input: {"a"=7, "x"=4, "z"=5} -> output: 2
      */
     public static int countOddNumbers(Map<String, Integer> map) {
-        return -1;
+        int amountOfOdd = 0;
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            if (entry.getValue() % 2 != 0) {
+                amountOfOdd++;
+            }
+        }
+        return amountOfOdd;
     }
     // --- Attempt 2 (With Resources, 10 min) ---
-    // Time Completed:
-    // Notes:
+    // Time Completed: 5:15 seconds remaining
+    // Notes: feeling good, I just need to remeber the correct syntax for looping though key value pairs.
 
 
     /**
@@ -93,11 +139,19 @@ public class MidtermPracticeSecondWithReasources {
      * Example 2: input: [3, 4, 6, 9] -> output: false
      */
     public static boolean allDivisibleBy3(int[] arr) {
-        return false;
+        int counter = 0;
+        for(int num: arr){
+            if (num % 3 == 0) {
+                counter++;
+            }
+        }
+        return counter == arr.length;
     }
        // --- Attempt 2 (With Resources, 10 min) ---
-    // Time Completed:
-    // Notes:
+    // Time Completed: 5:23 seconds remaining
+    // Notes: Could be more optimized but I am happy with my result.
+
+
 
     /**
      * Returns true if all words start with 'a' in an ArrayList of Strings.
